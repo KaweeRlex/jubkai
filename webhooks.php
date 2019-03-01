382c4b2128d8ee1fb70ff33fb810f4c1a0bfd226
KaweeRlex <48120234+KaweeRlex@users.noreply.github.com>
2019-03-01 16:39:00 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'PaD6A6Uv7hKNdPpqKbDEQvWKfYIrgqXbyh3gqYZ0ffcQWk2cv7goPbUz17iPENw3UuyHIStOB/Kmo/Ib3mtLx1EwFyW3a+e4IKUShCZMwBVCiFT0iXRM1c5SbfBZLvKSIfhaG+Sp9JzxNrlHnrSKfAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
